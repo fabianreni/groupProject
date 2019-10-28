658a0a69f0cddb65f995a314fa574dee98f0cac0
Adorjáni Árpád <arpika999@gmail.com>
2019-10-28 20:56:32 +0200

    Connecting activities and creating layouts
    Backend:
    -MainActivity (done)
    -ScrumPanel (started)
    Frontend:
    -listview for questions (done)
    -main.xml minor changes(done)
    -questions.xml (started)
    -scrum panel (delete one button is missing)
    -statistics (minor warning fixes)

--- app/src/main/java/com/example/planningpoker/MainActivity.java
+++ app/src/main/java/com/example/planningpoker/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.planningpoker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button joinBtn;
+    private Button scrumJoinBtn;
+    private EditText userNameET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Components();
+        JoinToQuestions();
+        JoinToScrumPanel();
     }
+
+    private void Components()
+    {
+        userNameET = findViewById(R.id.et_userName);
+        joinBtn = findViewById(R.id.b_join);
+        scrumJoinBtn = findViewById(R.id.b_scrum);
+    }
+
+
+    private void JoinToQuestions()
+    {
+        joinBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, QuestionsActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+
+    private void JoinToScrumPanel()
+    {
+        scrumJoinBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, ScrumPanelActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/planningpoker/ScrumPanelActivity.java
+++ app/src/main/java/com/example/planningpoker/ScrumPanelActivity.java
@@ -3,12 +3,39 @@ package com.example.planningpoker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ScrumPanelActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrum_panel);
+
+        Components();
+//        FillListView();
     }
+
+    private void Components()
+    {
+        listView = findViewById(R.id.simpleListView);
+    }
+
+
+    //UNDER CONSTRUCTION
+//    private void FillListView()
+//    {
+//        //DBHandler db = new DBHandler(this);
+//        //ArrayList<String> array = db.getHobbies();
+//
+//
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.activity_listview, R.id.textView,array);
+//        listView.setAdapter(arrayAdapter);
+//    }
+
 }
